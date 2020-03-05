8404b55e7572035647c30a8d6d9e3c343fc578b3
Niels Andriesse <andriesseniels@gmail.com>
2020-03-05 11:51:55 +1100

    Propagate Loki API related errors

--- java/src/main/java/org/whispersystems/signalservice/api/messages/SendMessageResult.java
+++ java/src/main/java/org/whispersystems/signalservice/api/messages/SendMessageResult.java
@@ -11,21 +11,26 @@ public class SendMessageResult {
   private final boolean              networkFailure;
   private final boolean              unregisteredFailure;
   private final IdentityFailure      identityFailure;
+  private final Throwable            exception;
 
   public static SendMessageResult success(SignalServiceAddress address, boolean unidentified, boolean needsSync) {
-    return new SendMessageResult(address, new Success(unidentified, needsSync), false, false, null);
+    return new SendMessageResult(address, new Success(unidentified, needsSync), false, false, null, null);
+  }
+
+  public static SendMessageResult exception(SignalServiceAddress address, Throwable exception) {
+      return new SendMessageResult(address, null, false, false, null, exception);
   }
 
   public static SendMessageResult networkFailure(SignalServiceAddress address) {
-    return new SendMessageResult(address, null, true, false, null);
+    return new SendMessageResult(address, null, true, false, null, null);
   }
 
   public static SendMessageResult unregisteredFailure(SignalServiceAddress address) {
-    return new SendMessageResult(address, null, false, true, null);
+    return new SendMessageResult(address, null, false, true, null, null);
   }
 
   public static SendMessageResult identityFailure(SignalServiceAddress address, IdentityKey identityKey) {
-    return new SendMessageResult(address, null, false, false, new IdentityFailure(identityKey));
+    return new SendMessageResult(address, null, false, false, new IdentityFailure(identityKey), null);
   }
 
   public SignalServiceAddress getAddress() {
@@ -48,12 +53,15 @@ public class SendMessageResult {
     return identityFailure;
   }
 
-  private SendMessageResult(SignalServiceAddress address, Success success, boolean networkFailure, boolean unregisteredFailure, IdentityFailure identityFailure) {
+  public Throwable getException() { return exception; }
+
+  private SendMessageResult(SignalServiceAddress address, Success success, boolean networkFailure, boolean unregisteredFailure, IdentityFailure identityFailure, Throwable exception) {
     this.address             = address;
     this.success             = success;
     this.networkFailure      = networkFailure;
     this.unregisteredFailure = unregisteredFailure;
     this.identityFailure     = identityFailure;
+    this.exception           = exception;
   }
 
   public static class Success {
