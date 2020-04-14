d167b4f3d37f87b41d8e751c57bcc9b814abfdf8
nielsandriesse <andriesseniels@gmail.com>
2020-04-14 10:42:07 +1000

    Merge files

--- java/src/main/java/org/whispersystems/signalservice/loki/utilities/Promise.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/utilities/Promise.kt
@@ -1,10 +1,32 @@
 @file:JvmName("PromiseUtil")
 package org.whispersystems.signalservice.loki.utilities
 
-import nl.komponents.kovenant.Promise
-import nl.komponents.kovenant.deferred
+import nl.komponents.kovenant.*
 import org.whispersystems.libsignal.logging.Log
 import java.util.concurrent.TimeoutException
+import kotlin.math.max
+
+// Try to use all available threads minus one for the callback
+private val recommendedThreadCount: Int
+    get() = Runtime.getRuntime().availableProcessors() - 1
+
+fun Kovenant.createContext(contextName: String, threadCount: Int = max(recommendedThreadCount, 1)): Context {
+    return createContext {
+        callbackContext.dispatcher = buildDispatcher {
+            name = "${contextName}CallbackDispatcher"
+            // Ref: http://kovenant.komponents.nl/api/core_usage/#execution-order
+            // Having 1 concurrent task ensures we have in-order callback handling
+            concurrentTasks = 1
+        }
+        workerContext.dispatcher = buildDispatcher {
+            name = "${contextName}WorkerDispatcher"
+            concurrentTasks = threadCount
+        }
+        multipleCompletion = { lhs, rhs ->
+            Log.d("Loki", "Promise resolved more than once (first with $lhs, then with $rhs); ignoring $rhs.")
+        }
+    }
+}
 
 fun <V, E : Throwable> Promise<V, E>.get(defaultValue: V): V {
   return try {
