5ce51ce7f9190e69051878db8bf29b8f43b647df
nielsandriesse <andriesseniels@gmail.com>
2020-08-07 11:45:47 +1000

    Implement closed group sender key serialization & deserialization

--- java/src/main/java/org/whispersystems/signalservice/loki/protocol/closedgroups/ClosedGroupSenderKey.kt
+++ java/src/main/java/org/whispersystems/signalservice/loki/protocol/closedgroups/ClosedGroupSenderKey.kt
@@ -1,8 +1,32 @@
 package org.whispersystems.signalservice.loki.protocol.closedgroups
 
+import org.whispersystems.libsignal.logging.Log
+import org.whispersystems.libsignal.util.Hex
+import org.whispersystems.signalservice.internal.util.JsonUtil
 import org.whispersystems.signalservice.loki.utilities.toHexString
 
 public class ClosedGroupSenderKey(public val chainKey: ByteArray, public val keyIndex: Int, public val publicKey: ByteArray) {
+
+    companion object {
+
+        public fun fromJSON(jsonAsString: String): ClosedGroupSenderKey? {
+            try {
+                val json = JsonUtil.fromJson(jsonAsString, Map::class.java)
+                val chainKey = Hex.fromStringCondensed(json["chainKey"] as String)
+                val keyIndex = json["keyIndex"] as Int
+                val publicKey = Hex.fromStringCondensed(json["publicKey"] as String)
+                return ClosedGroupSenderKey(chainKey, keyIndex, publicKey)
+            } catch (exception: Exception) {
+                Log.d("Loki", "Couldn't parse closed group sender key from: $jsonAsString.")
+                return null
+            }
+        }
+    }
+
+    public fun toJSON(): String {
+        val json = mapOf( "chainKey" to chainKey.toHexString(), "keyIndex" to keyIndex, "publicKey" to publicKey.toHexString() )
+        return JsonUtil.toJson(json)
+    }
 
     override fun equals(other: Any?): Boolean {
         return if (other is ClosedGroupSenderKey) {
